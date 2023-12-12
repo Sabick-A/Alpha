95ee88d95b17e3962da1c4f33841afd99d555a5f
Sabick A <sabicksabi@gmail.com>
2023-12-12 14:42:34 +0530

    Chapter 6 completed

---
+++ 6.var_and_datatype/Q1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q1 {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        double average=(a+b+c)/3.0;
+        System.out.println(average);
+        sc.close();
+    }
+}
---
+++ 6.var_and_datatype/Q3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float pencil=sc.nextFloat();
+        float pen=sc.nextFloat();
+        float eraser=sc.nextFloat();
+        float total=pencil+pen+eraser;
+        total+=(total*0.18f);
+        System.out.println(total);
+        sc.close();
+    }
+}
